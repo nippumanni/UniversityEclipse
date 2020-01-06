2ca6ecbb868d9faaf0dfd48eee12e858c0d11087
Dalesh Prashar <prashdale@gmail.com>
2020-01-05 18:54:24 -0600

    created justToCheck

---
+++ universityPost/src/main/java/JustToCheck.java
@@ -0,0 +1,19 @@
+
+public class JustToCheck {
+String boogie;
+String Woogie;
+public String getBoogie() {
+	return boogie;
+}
+public void setBoogie(String boogie) {
+	this.boogie = boogie;
+}
+public String getWoogie() {
+	return Woogie;
+}
+public void setWoogie(String woogie) {
+	Woogie = woogie;
+}
+
+
+}
